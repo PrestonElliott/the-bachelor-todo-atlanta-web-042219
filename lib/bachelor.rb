bd2f2907f8182a07b7a821586805fcd934c72ac4
Preston Elliott <preston.elliott24@gmail.com>
2019-04-16 11:58:20 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,7 @@ def get_first_name_of_season_winner(data, season)
   # return first name of season winner
   data[season].each do |contestant_data|
     if contestant_data["status"].downcase == "winner"
-      puts contestant_data["name"].split(" ").first
+      return contestant_data["name"].split(" ").first
     end
   end
 end
