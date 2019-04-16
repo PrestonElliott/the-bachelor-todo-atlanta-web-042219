850a23ac9ea5c793297dffc6bf8bdc9ea33deec0
Preston Elliott <preston.elliott24@gmail.com>
2019-04-16 12:15:00 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -44,6 +44,8 @@ end
 
 def get_average_age_for_season(data, season)
   # return average age of all contestants from a season using float
+  avg_age = 0
+  contestant_count = 0
   data.each do |season, contestant|
     contestant.each do |contestant_data|
       if contestant_data["season"]
